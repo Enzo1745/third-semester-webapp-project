1657dc675981fa95d9bfbcf5e38c156f3b0a98cd
Martin Pomas <mpomas@dimm08.univ-lr.fr>
2024-11-07 12:08:41 +0100

    feat(Connection): ajout d'une page de connection avec une redirection vers une page de indiquant le succes de la connection

--- sfapp/migrations/Version20241107093128.php
+++ sfapp/migrations/Version20241107093128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241106163935 extends AbstractMigration
+final class Version20241107093128 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241106163935 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, nom_utilisateur VARCHAR(255) NOT NULL, mot_de_passe VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('DROP TABLE utilisateur');
     }
 }
--- sfapp/src/Controller/ConnectionController.php
+++ sfapp/src/Controller/ConnectionController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Form\ConnectionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConnectionController extends AbstractController
 {
     #[Route('/connection', name: 'app_connection')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('connection/index.html.twig', []);
+        $form = $this->createForm(ConnectionType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            return $this->redirectToRoute('app_connection_sucess');
+        }
+        return $this->render('connection/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+}
+
+    #[Route('/connection/done', name: 'app_connection_sucess')]
+    public function connectionReussie(): Response
+    {
+        return $this->render('connection/succes.html.twig');
     }
 }
