71eb5238651720787365d6eeffc54695cca5b972
Enzo Pereira <eperei03@dimm06.univ-lr.fr>
2024-11-13 12:13:38 +0100

    [test](default test)

---
+++ sfapp/tests/DisplayRoomsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DisplayRoomsTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+
+
+}
