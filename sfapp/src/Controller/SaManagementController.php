88d8337e2ab7a11e82a27b49421c86270994adf5
Vincent Thiollet <vincent.thiollet@etudiant.univ-lr.fr>
2024-11-14 16:25:26 +0100

    [fix](Correction des chemins d'accès)

--- sfapp/src/Controller/SaManagementController.php
+++ sfapp/src/Controller/SaManagementController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SaManagementController extends AbstractController
 {
-    #[Route('charge/gestion_sa', name: 'app_gestion_sa')]
+    #[Route('charge/gestion_sa/associer', name: 'app_gestion_sa')]
     public function index(Request $request, EntityManagerInterface $manager, SaRepository $saRepo, RoomRepository $salleRepo): Response
     {
         $nbSa = $saRepo->countBySaState();
