139a64ef4f5af81a692ef21e06744978c2914323
Vincent Thiollet <vthiolle@dimm04.univ-lr.fr>
2024-11-14 11:56:38 +0100

    B [feat](Création de la liste des sa + gestion de son affichage)

---
+++ sfapp/src/Entity/Sa.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Model\SaState;
+use App\Repository\SaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SaRepository::class)]
+class Sa
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(enumType: SaState::class)]
+    private ?SaState $etat = null;
+
+    #[ORM\OneToOne(inversedBy: 'sa', cascade: ['persist', 'remove'])]
+    private ?Salle $salle = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtat(): ?SaState
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(SaState $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getSalle(): ?Salle
+    {
+        return $this->salle;
+    }
+
+    public function setSalle(?Salle $salle): static
+    {
+        $this->salle = $salle;
+
+        return $this;
+    }
+
+
+}
