d059d5192a9725fb143eeb1fa6fcc805bcd094dc
Vincent Thiollet <vincent.thiollet@etudiant.univ-lr.fr>
2024-11-14 16:16:35 +0100

    [feat](Ajout de tous les tests)

---
+++ sfapp/tests/DisplaySaTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\Model\SaState;
+use App\Entity\Salle;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Sa;
+
+class DisplaySaTest extends WebTestCase
+{
+    private $client;
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+        $this->entityManager = $this->client->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testListRoute(): void
+    {
+        $this->client->request('GET', '/charge/gestion_sa');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testDisplayListOfSaWithSa(): void
+    {
+        $room = new Salle();
+        $room->setNomSalle("D204");
+        $this->entityManager->persist($room);
+
+        $sa = new Sa();
+        $sa->setEtat(SaState::Available);
+        $sa->setSalle($room);
+        $this->entityManager->persist($sa);
+
+        $this->entityManager->flush();
+
+        $this->client->request('GET', '/charge/gestion_sa');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('.idSa');
+        $this->assertSelectorTextContains('.roomName', "D204");
+    }
+
+    public function testDisplayListOfSaWithoutSa(): void
+    {
+        $this->entityManager->createQuery('DELETE FROM App\Entity\Sa')->execute();
+        $this->entityManager->createQuery('DELETE FROM App\Entity\Salle')->execute();
+
+        $this->client->request('GET', '/charge/gestion_sa');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorNotExists('.tab');
+        $this->assertSelectorExists('p', 'Aucun système d\'acquisition n\'est disponible.');
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->entityManager) {
+            $this->entityManager->close();
+            $this->entityManager = null;
+        }
+        $this->client = null;
+        parent::tearDown();
+    }
+}
