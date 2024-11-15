81f02ddca2b58045ddd2a962bf2c24754f811728
Vincent Thiollet <vthiolle@ssd02.univ-lr.fr>
2024-11-15 10:06:01 +0100

    [fix | feat](Correction du merge + ajout navigation entre les pages)

--- sfapp/migrations/Version20241115085033.php
+++ sfapp/migrations/Version20241115085033.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241113230600 extends AbstractMigration
+final class Version20241115085033 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20241113230600 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE sa (id INT AUTO_INCREMENT NOT NULL, salle_id INT DEFAULT NULL, etat VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7F7E6904DC304035 (salle_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE salle (id INT AUTO_INCREMENT NOT NULL, nomSalle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE sa ADD CONSTRAINT FK_7F7E6904DC304035 FOREIGN KEY (salle_id) REFERENCES salle (id)');
     }
 
@@ -28,6 +28,6 @@ final class Version20241113230600 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sa DROP FOREIGN KEY FK_7F7E6904DC304035');
-        $this->addSql('DROP TABLE sa');
+        $this->addSql('DROP TABLE salle');
     }
 }
