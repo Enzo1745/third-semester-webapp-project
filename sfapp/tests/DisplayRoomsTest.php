afeb684a1af15b5ea09404ddad0ea5c357d0469e
Tiago Barbosa -Neau <tbarbosa@ssd10.univ-lr.fr>
2024-11-15 09:58:04 +0100

    [test](Ajout de 3 tests pour l'US

--- sfapp/tests/DisplayRoomsTest.php
+++ sfapp/tests/DisplayRoomsTest.php
@@ -2,18 +2,74 @@
 
 namespace App\Tests;
 
+use App\Entity\Sa;
+use App\Entity\Salle;
+use App\Repository\Model\EtatSA;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DisplayRoomsTest extends WebTestCase
 {
-    public function test(): void
+    public function testSearchingDetailsOfAssociatedSAWithData(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', 'charge/salles/liste');
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $sa = new Sa();
+        $sa->setEtat(EtatSA::Fonctionnel);
+        $sa->setTemperature(25);
+        $sa->setHumidite(60);
+        $sa->setCO2(1200);
+
+        $salle = new Salle();
+        $salle->setNumSalle("D101");
+        $salle->setIdSA($sa);
+
+        $entityManager = self::getContainer()->get('doctrine')->getManager();
+        $entityManager->persist($salle);
+        $entityManager->persist($sa);
+        $entityManager->flush();
+
+        $crawler = $client->request('GET', '/charge/salles/liste/D101');
+
+        $this->assertSelectorTextContains('.value.temp', '25°');
+        $this->assertSelectorTextContains('.value.humidity', '60%');
+        $this->assertSelectorTextContains('.value.co2', '1200');
     }
 
+    public function testSearchingDetailsOfAssociatedSAWithNoData(): void
+    {
+        $client = static::createClient();
+
+        $sa = new Sa();
+        $sa->setEtat(EtatSA::Fonctionnel);
+
+        $salle = new Salle();
+        $salle->setNumSalle("D301");
+        $salle->setIdSA($sa);
+
+        $entityManager = self::getContainer()->get('doctrine')->getManager();
+        $entityManager->persist($salle);
+        $entityManager->persist($sa);
+        $entityManager->flush();
+
+        $crawler = $client->request('GET', '/charge/salles/liste/D301');
+
+        $this->assertSelectorTextContains('.salle-details', 'Aucunes données');
+    }
+
+    public function testSearchingDetailsOfSalleWithoutSA(): void
+    {
+        $client = static::createClient();
+
+        $salle = new Salle();
+        $salle->setNumSalle("D303");
+
+        $entityManager = self::getContainer()->get('doctrine')->getManager();
+        $entityManager->persist($salle);
+        $entityManager->flush();
+
+        $crawler = $client->request('GET', '/charge/salles/liste');
+
+        $this->assertSelectorTextContains('.noData', 'Aucun SA associé');
+    }
 
 }
