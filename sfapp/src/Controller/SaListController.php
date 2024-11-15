bec3803d4c815a570791ba03c6515875e0fea730
Vincent Thiollet <vthiolle@ssd02.univ-lr.fr>
2024-11-15 08:52:21 +0100

    [docs](Ajout de la documentation)

--- sfapp/src/Controller/SaListController.php
+++ sfapp/src/Controller/SaListController.php
@@ -10,12 +10,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * @brief Controller of the list of sa
+ */
 class SaListController extends AbstractController
 {
+    /**
+     * @brief Function to return the list of sa in the web page
+     * @param Request $request
+     * @param SaRepository $saRepo
+     * @return Response -> return the web page with the list of all sa
+     */
     #[Route('/charge/gestion_sa', name: 'app_gestion_sa')]
     public function listSA(Request $request, SaRepository $saRepo): Response
     {
-        $saList = $saRepo->findAll();
+        $saList = $saRepo->findAll(); // Function used to return all the sa in the database.
 
         return $this->render('gestion_sa/list.html.twig', [
             "saList" => $saList
