489427d4daed2420163cb7f0f96f2a550953ede1
Vincent Thiollet <vthiolle@ssd02.univ-lr.fr>
2024-11-15 08:52:21 +0100

    [docs](Ajout de la documentation)

--- sfapp/src/DataFixtures/AppFixtures.php
+++ sfapp/src/DataFixtures/AppFixtures.php
@@ -8,8 +8,16 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Salle;
 use App\Entity\Sa;
 
+/**
+ * @brief Class containing all the default data for the database
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * @brief Load all the default data in the database
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
         $salle1 = new Salle();
@@ -30,12 +38,14 @@ class AppFixtures extends Fixture
         $sa3 = new Sa();
         $sa3->setEtat(SaState::Available);
 
+        // Link the data created with our manager
         $manager->persist($salle1);
         $manager->persist($sa1);
         $manager->persist($salle2);
         $manager->persist($sa2);
         $manager->persist($sa3);
 
+        // Write the linked data in our database
         $manager->flush();
     }
 }
