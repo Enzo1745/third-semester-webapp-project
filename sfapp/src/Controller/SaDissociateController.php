37fe45cec90c0f41b9d0247d4fa6ae5e1ba29f10
Vincent Thiollet <vthiolle@dimm04.univ-lr.fr>
2024-11-18 16:57:20 +0100

    [doc](Ajout de la documentation)

--- sfapp/src/Controller/SaDissociateController.php
+++ sfapp/src/Controller/SaDissociateController.php
@@ -14,14 +14,18 @@ class SaDissociateController extends AbstractController
     #[Route('/sa/dissociate/{id}', name: 'app_sa_dissociate', methods: ['POST'])]
     public function dissociate(int $id, SaRepository $saRepo, EntityManagerInterface $entityManager): Response
     {
+        // Get the sa with the $id
         $sa = $saRepo->find($id);
-        if ($sa AND $sa->getRoom() != null) {
+
+        if ($sa AND $sa->getRoom() != null) { // Test that the sa has a room associated
+
+            // Dissociate the room and reset the sa's state
             $sa->setRoom(null);
             $sa->setState(SaState::Available);
             $entityManager->persist($sa);
             $entityManager->flush();
         }
-
+        // Redirect to the sa list page
         return $this->redirectToRoute('app_gestion_sa');
     }
 }
