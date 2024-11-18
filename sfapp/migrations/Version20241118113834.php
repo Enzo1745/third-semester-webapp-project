f3d5a543054d78388bfe6dd93d97fa13e1c418b4
Vincent Thiollet <vthiolle@ssd10.univ-lr.fr>
2024-11-18 13:19:23 +0100

    Ajout des fichiers de test

--- sfapp/migrations/Version20241118113834.php
+++ sfapp/migrations/Version20241118113834.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241115165509 extends AbstractMigration
+final class Version20241118113834 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,16 @@ final class Version20241115165509 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, roomName VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sa (id INT AUTO_INCREMENT NOT NULL, room_id INT DEFAULT NULL, state VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7F7E690454177093 (room_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE sa ADD CONSTRAINT FK_7F7E690454177093 FOREIGN KEY (room_id) REFERENCES room (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_7F7E690454177093 ON sa (room_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sa DROP FOREIGN KEY FK_7F7E690454177093');
-        $this->addSql('DROP INDEX UNIQ_7F7E690454177093 ON sa');
+        $this->addSql('DROP TABLE room');
+        $this->addSql('DROP TABLE sa');
     }
 }
