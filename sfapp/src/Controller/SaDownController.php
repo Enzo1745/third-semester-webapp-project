51d667da3552c89883887bf7c3b8ec186547d688
Vincent Thiollet <vthiolle@dimm04.univ-lr.fr>
2024-12-09 08:53:20 +0100

    [Doc](Ajout de la documentation complète du programme)

--- sfapp/src/Controller/SaDownController.php
+++ sfapp/src/Controller/SaDownController.php
@@ -16,32 +16,48 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SaDownController extends AbstractController
 {
+
+    /**
+     * @param DownRepository $saDownRepo
+     * @param SaRepository $saRepo
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     * @throws \Exception
+     */
     #[Route('/technicien/sa/panne', name: 'app_down')]
     public function addDown(DownRepository $saDownRepo, SaRepository $saRepo, Request $request, EntityManagerInterface $entityManager): Response
     {
+        // Create a new down
         $newDown = new Down();
 
+        // Create the form for the new down
         $form = $this->createForm(SaDownType::class, $newDown);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($newDown->getSa() != null && (
-                    $newDown->isCO2() || $newDown->isHumidity() || $newDown->isTemperature() || $newDown->isMicrocontroller()) &&
-                $newDown->getReason() != null) {
+            if ($newDown->getSa() != null && ( // If the selected SA exists
+                    $newDown->isCO2() || $newDown->isHumidity() || $newDown->isTemperature() || $newDown->isMicrocontroller()) && // And at least one captor or the microcontroller is selected
+                $newDown->getReason() != null) { // And the reason is filled up
 
+                // Set the down date
                 $newDown->setDate(new \DateTime('now', new \DateTimeZone('Europe/Paris')));
+
+                // Set the selected SA' state to down
                 $newDown->getSa()->setState(SAState::Down);
 
+                // Insert the new down in the database
                 $entityManager->persist($newDown);
                 $entityManager->flush();
 
+                // Return a message when the operation succeed
                 $this->addFlash('success', 'Le SA est désormais dysfonctionnel');
                 return $this->redirect("#");
             }
         }
 
-        $saList = $saDownRepo->findLastDownSa(); // Utiliser la méthode modifiée
-        $nbSaFunctionnals = $saRepo->countBySaState(SAState::Functional);
+        $saList = $saDownRepo->findLastDownSa(); // Get the list of all down sa
+        $nbSaFunctionnals = $saRepo->countBySaState(SAState::Functional); // Get the number of functioning SA
 
         return $this->render('sa_down/sa_down.html.twig', [
             "nbSaFunctionnals" => $nbSaFunctionnals,
@@ -50,13 +66,22 @@ class SaDownController extends AbstractController
         ]);
     }
 
+    /**
+     * @param Sa $sa
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @return Response
+     */
     #[Route('/technicien/sa/panne/{id}', name: 'app_functionnal')]
     public function setFunctionnal(Sa $sa, EntityManagerInterface $entityManager, Request $request): Response
     {
+        // Set the selected sa' state to functional
         $sa->setState(SAState::Functional);
 
+        // Update the database
         $entityManager->flush();
 
+        // Display a success message
         $this->addFlash('success', 'Le SA a été réhabilité avec succès.');
 
         return $this->redirectToRoute('app_down');
