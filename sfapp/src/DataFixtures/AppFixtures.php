6efb7e9e0112e79afdf29f8c6cac2f7ff9a9d22f
mmasquel <maxime.masquelier@etudiant.univ-lr.fr>
2024-12-10 00:44:13 +0100

    [feat](ajout sa_salle association fonctionnel)

--- sfapp/src/DataFixtures/AppFixtures.php
+++ sfapp/src/DataFixtures/AppFixtures.php
@@ -15,9 +15,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
-        //normes
-        // Norm summer
+        // Création des normes (Summer, Winter)
         $summerNorm = new Norm();
         $summerNorm->setSeason('summer')
             ->setHumidityMinNorm(30)
@@ -28,7 +26,6 @@ class AppFixtures extends Fixture
             ->setCo2MaxNorm(1000);
         $manager->persist($summerNorm);
 
-        // Norm winter
         $winterNorm = new Norm();
         $winterNorm->setSeason('winter')
             ->setHumidityMinNorm(40)
@@ -39,97 +36,93 @@ class AppFixtures extends Fixture
             ->setCo2MaxNorm(900);
         $manager->persist($winterNorm);
 
-        // Création des SAs sans salle associée
+        // Création des systèmes d'acquisition (SA) avec différents statuts
+        // SA1 - Available
         $sa1 = new Sa();
-        $sa1->setState(SAState::Available);
-        $sa1->setCO2(4000);
-        $sa1->setHumidity(51);
-        $sa1->setTemperature(19);
+        $sa1->setState(SAState::Available)
+            ->setCO2(4000)
+            ->setHumidity(51)
+            ->setTemperature(19);
         $manager->persist($sa1);
 
-
-
+        // SA2 - Functional
         $sa2 = new Sa();
-        $sa2->setState(SAState::Available);
-        $sa2->setCO2(4800);
-        $sa2->setHumidity(50);
-        $sa2->setTemperature(16);
+        $sa2->setState(SAState::Functional)
+            ->setCO2(4800)
+            ->setHumidity(50)
+            ->setTemperature(16);
         $manager->persist($sa2);
 
-        // Création des salles
+        // SA3 - Down (En panne)
+        $sa3 = new Sa();
+        $sa3->setState(SAState::Down)
+            ->setCO2(5000)
+            ->setHumidity(47)
+            ->setTemperature(19);
+        $manager->persist($sa3);
+
+        // SA4 - Waiting (En attente)
+        $sa4 = new Sa();
+        $sa4->setState(SAState::Waiting)
+            ->setCO2(4600)
+            ->setHumidity(48)
+            ->setTemperature(17);
+        $manager->persist($sa4);
+
+        // Création des salles et association en fonction des statuts
+        // Salle D204 - associée à SA1 (Disponible)
         $room1 = new Room();
-        $room1->setRoomName("D204");
-        $room1->setNbWindows(2);
-        $room1->setNbRadiator(1);
+        $room1->setRoomName("D204")
+            ->setNbWindows(2)
+            ->setNbRadiator(1);
+        $room1->setSa($sa1);  // Association avec SA1 (Disponible)
         $manager->persist($room1);
 
-
-
-        $sa3 = new Sa();
-        $sa3->setState(SAState::Available);
-        $manager->persist($sa3);
-
+        // Salle D205 - associée à SA2 (Fonctionnel)
         $room2 = new Room();
-        $room2->setRoomName("D205");
-        $room2->setNbWindows(2);
-        $room2->setNbRadiator(1);
+        $room2->setRoomName("D205")
+            ->setNbWindows(2)
+            ->setNbRadiator(1);
+        $room2->setSa($sa2);  // Association avec SA2 (Fonctionnel)
         $manager->persist($room2);
 
-
-        $sa3 = new Sa();
+        // Salle D206 - associée à SA3 (En panne) - ne doit pas être associée à une salle fonctionnelle
         $room3 = new Room();
-        $room3->setRoomName("D206");
-        $room3->setNbWindows(4);
-        $room3->setNbRadiator(2);
-
-        $room3->setSa($sa3);
-        $sa3->setState(SAState::Functional);
-        $manager->persist($sa3);
+        $room3->setRoomName("D206")
+            ->setNbWindows(4)
+            ->setNbRadiator(2);
+        $room3->setSa($sa3);  // Association avec SA3 (En panne), mais cette salle ne doit pas être utilisée
         $manager->persist($room3);
 
+        // Salle D304 - associée à SA4 (En attente) - ne doit pas être associée à une salle fonctionnelle
         $room4 = new Room();
-        $room4->setRoomName("D304");
-        $room4->setNbWindows(5);
-        $room4->setNbRadiator(3);
+        $room4->setRoomName("D304")
+            ->setNbWindows(5)
+            ->setNbRadiator(3);
+        $room4->setSa($sa4);  // Association avec SA4 (En attente)
         $manager->persist($room4);
 
+        // Salle D306 - sans SA (En attente d'association)
+        $room5 = new Room();
+        $room5->setRoomName("D306")
+            ->setNbWindows(4)
+            ->setNbRadiator(2);
+        $manager->persist($room5);
+
+        // Création des utilisateurs
         $user1 = new User();
-        $user1->setUsername('chargé');
-        $user1->setPassword('1234');
-        $user1->setRole(UserRoles::Charge);
+        $user1->setUsername('chargé')
+            ->setPassword('1234')
+            ->setRole(UserRoles::Charge); // Role chargé
         $manager->persist($user1);
 
         $user2 = new User();
-        $user2->setUsername('tech');
-        $user2->setPassword('5678');
-        $user2->setRole(UserRoles::Technicien);
+        $user2->setUsername('tech')
+            ->setPassword('5678')
+            ->setRole(UserRoles::Technicien); // Role technicien
         $manager->persist($user2);
 
-
-        $sa5 = new Sa();
-        $room5 = new Room();
-        $room5->setRoomName("D306");
-        $room5->setNbWindows(4);
-        $room5->setNbRadiator(2);
-
-        $room5->setSa($sa3);
-        $sa5->setState(SAState::Functional);
-        $manager->persist($sa5);
-        $manager->persist($room5);
-
-        $sa6 = new Sa();
-        $room6 = new Room();
-        $room6->setRoomName("D305");
-        $room6->setNbWindows(4);
-        $room6->setNbRadiator(2);
-
-        $room6->setSa($sa6);
-        $sa6->setState(SAState::Functional);
-        $manager->persist($sa6);
-        $manager->persist($room6);
-
-
-
+        // Sauvegarde de tout dans la base de données
         $manager->flush();
     }
 }
