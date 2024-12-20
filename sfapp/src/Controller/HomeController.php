aefe044c8d1942a93145b0ee5f0b4480dc5526a8
eperei03 <eperei03@etudiant.univ-lr.fr>
2024-12-20 15:15:50 +0100

    [fix]: résolution du bug de tâches qui réapparaissent

---
+++ sfapp/src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/{roomName}', name: 'app_home_room_info')]
+    public function index(string $roomName, RoomRepository $roomRepository): Response
+    {
+        $room = $roomRepository->findByRoomName($roomName);
+
+        return $this->render('home/room.html.twig', [
+            'room' => $room
+        ]);
+    }
+}
