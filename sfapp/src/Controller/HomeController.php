8511b3d45bccfda81b2520f35741121b87d1f916
Martin Pomas <mpomas@dimm06.univ-lr.fr>
2025-01-06 11:42:56 +0100

    [feat](ajout des informations des salles dans la page d'acceuil): Ajout d'un menu deroulant de selection des salles et de la section de visualisation des données de la salle selectionnée ainsi qu'un visuel par default quand aucune salle est selectionnée

--- sfapp/src/Controller/HomeController.php
+++ sfapp/src/Controller/HomeController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Norm;
+use App\Entity\Room;
+use App\Entity\Sa;
+use App\Form\SearchRoomsType;
+use App\Repository\DownRepository;
+use App\Repository\Model\SAState;
+use App\Repository\RoomRepository;
 use App\Repository\TipsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,13 +21,57 @@ class HomeController extends AbstractController
     /**
      * @brief default page
      */
+
+
     #[Route('/', name: 'app_home')]
-    public function index(TipsRepository $tipsRepo ): Response
+    public function roomInfo(
+        Request $request,
+        RoomRepository $roomRepository,
+        DownRepository $downRepo,
+        EntityManagerInterface $entityManager,
+        TipsRepository $tipsRepo
+    ): Response
     {
         $tips = $tipsRepo->findRandTips(); // return a random tips from database
 
+        $form = $this->createForm(SearchRoomsType::class);
+
+        $form->handleRequest($request);
+
+        $roomName = '';
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedRoom = $form->get('salle')->getData();
+            if ($selectedRoom) {
+                $roomName = $selectedRoom->getRoomName();
+            }
+        }
+
+        $room = $roomRepository->findByRoomName($roomName);
+        $down = null;
+
+        $normRepository = $entityManager->getRepository(Norm::class);
+        $norms = $normRepository->findOneBy([
+            'NormType' => 'confort',
+            'NormSeason' => 'été'
+        ]);
+
+        $sa = null;
+        if ($room and $room->getIdSA()) {
+            $sa = $entityManager->getRepository(Sa::class)->find($room->getIdSA());
+            if ($sa && $sa->getState() == SAState::Down) {
+                $down = $downRepo->findOneBy(['sa' => $sa]);
+            }
+        }
+
         return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
             'tips' => $tips,
+            'room' => $room,
+            'sa' => $sa,
+            'origin' => 'charge',
+            'norms' => $norms,
+            'down' => $down,
         ]);
     }
 }
---
+++ sfapp/src/Form/SearchRoomsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Room;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchRoomsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('salle', EntityType::class, [
+                'class' => Room::class,
+                'choice_label' => 'roomName',
+                'placeholder' => 'Sélectionner une salle',
+                'label' => 'Choisir une salle :',
+                'required' => true,
+                'attr' => [
+                    'onchange' => 'this.form.submit()',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
